c770e441bc9aaed26b7a9756f6c0ad911cfdbc64
Afanasev Mikhail <miafanasev@gmail.com>
2024-08-05 22:33:45 +0700

    Удалён лишний код

--- src/main/java/ru/dl/Starter.java
+++ src/main/java/ru/dl/Starter.java
@@ -9,9 +9,5 @@ import ru.dl.repo.AccountRepo;
 public class Starter {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Starter.class, args);
-        System.out.println("Привет, Мир!");
-
-        AccountRepo accountRepo = ctx.getBean(AccountRepo.class);
-        accountRepo.findAll().stream().forEach(System.out::println);
     }
 }
